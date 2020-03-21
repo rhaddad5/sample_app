8ec9dec1733266453b9232e79389d34c1e5fae72
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-21 16:17:19 +0100

    a few stuff

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get root_path
-    assert_response :success
-    assert_select "title", "Home page | Sample App"
-  end
+    test "should get home" do
+      get root_path
+      assert_response :success
+      assert_select "title", "Home page | Sample App"
+    end
 
   test "should get help" do
     get help_path
