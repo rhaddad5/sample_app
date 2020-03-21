99c82df65cadcb70c9e365dc5c3c41e18eeafa67
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-21 15:44:26 +0100

    added contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About page | Sample App"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact page | Sample App"
+  end
+
 end
