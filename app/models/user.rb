6d7480edf0fa4175a0d328c70cec914f62b7fb40
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-22 17:04:27 +0100

    finished with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
   validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :password, presence: true, length: {minimum: 6}
   has_secure_password
 end
