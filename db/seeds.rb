488f45c6cf826995f41bfebd435092714ab81298
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-23 16:28:18 +0100

    creating signup tests and made a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!(name: "Roxane Haddad", email: "roxane.haddad@gmail.com", password: "123456", password_confirmation: "123456")
