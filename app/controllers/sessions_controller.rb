205f89bee2e5f998acba3e854a0c006333762304
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-25 19:25:17 +0100

    still working on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:user][:email].downcase)
     if user && user.authenticate(params[:user][:password])
+      log_in(user)
       redirect_to user_path(user)
     else
       flash.now[:alert] = "Invalid email/password combination"
