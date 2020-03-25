fa200a904d5c59e2a2922456659424bc2afcb770
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-25 19:49:18 +0100

    created the current_user helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
 
   def current_user
     if session[:user_id]
-      User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 end
