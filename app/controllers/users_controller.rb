1536ade996d73aa4d06cda5f8f74704b1d2a185f
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-29 19:37:25 +0200

    implemented authorization to make sure user can only change his info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -44,5 +45,10 @@ class UsersController < ApplicationController
       flash[:alert] = "Please log in."
       redirect_to login_path
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
   end
 end
