d48c5f3275f754959c26ab17115ac86ae7c79c0b
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-01 19:15:22 +0200

    account activation done, just tests and refacto left

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,15 @@
 class AccountActivationsController < ApplicationController
   def edit
+    @user = User.find_by(email: params[:email])
+    if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+      @user.update_attribute(:activated, true)
+      @user.update_attribute(:activated_at, Time.zone.now)
+      log_in @user
+      flash[:notice] = "Your account was successfully activated!"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Invalid activation link"
+      redirect_to root_path
+    end
   end
 end
