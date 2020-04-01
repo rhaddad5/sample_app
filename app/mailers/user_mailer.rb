5a8fa5b5194fd9f4aa6837609e6bacf2e2c7287b
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-01 18:09:10 +0200

    finished the activation emails implementation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.account_activation.subject
   #
   def account_activation(user)
-    @greeting = "Hi"
+    @user = user
     mail to: user.email, subject: "Account activation"
   end
 
