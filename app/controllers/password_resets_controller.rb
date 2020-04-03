1c0b9077bd1e7459e905a63ec84640548b0c63d9
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-03 18:53:22 +0200

    password reset functionnality should work

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,13 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
   def create
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    @user = User.find_by(email: params[:user][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -16,6 +20,38 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+    elsif @user.update(user_params)
+      log_in(@user)
+      flash[:notice] = "Your password has been reset."
+      redirect_to user_path(@user)
+    else
+      render "edit"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+      redirect_to root_path
+    end
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:alert] = "Password reset has expired."
+      redirect_to new_password_reset_path
+    end
   end
 end
