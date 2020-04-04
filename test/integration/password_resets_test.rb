fb15b11066bc4c8617158ed5abd673907a06c036
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-04 15:15:31 +0200

    finished tests but one test not working

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -10,7 +10,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
   test "password resets" do
     get new_password_reset_path
     assert_template "password_resets/new"
-    assert_select "input[name=?], password_reset[email]"
+    assert_select "input[name=?]", "user[email]"
     post password_resets_path, params: {user: {email: ""}}
     assert_not flash.empty?
     assert_template "password_resets/new"
@@ -19,5 +19,20 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirected_to root_path
+    user = assigns(:user)
+    get edit_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to root_path
+    user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to root_path
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_template "password_resets/edit"
+    assert_select "input[name=email][type=hidden][value=?] ", user.email
+    patch password_reset_path(user.reset_token), params: {user: {email: user.email, password: "foobaz", password_confirmation: "barquux"}}
+    assert_select "div.invalid-feedback"
+    patch password_reset_path(user.reset_token), params: {user: {email: user.email, password: "foobaz", password_confirmation: "foobaz"}}
+    assert user.is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user_path(user)
   end
 end
