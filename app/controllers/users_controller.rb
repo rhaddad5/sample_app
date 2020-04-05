c0255c233ff917bbf2c25c65ca9212504854f67f
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-05 16:55:51 +0200

    added tests and before action for creating and destroying microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:alert] = "Please log in."
-      redirect_to login_path
-    end
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_path unless current_user?(@user)
