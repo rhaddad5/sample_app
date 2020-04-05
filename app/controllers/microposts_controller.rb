4825942864ef83795d72b60aec1524c3be141ae0
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-05 20:04:55 +0200

    added current_user feed on home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,22 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:notice] = "Your new micropost was successfully created!"
+      redirect_to root_path
+    else
+      @feed_items = current_user.microposts.paginate(page: params[:page])
+      render "static_pages/home"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
