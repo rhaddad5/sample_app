b0921203ebf788e706e7e3c09353a0de0ec0e6d9
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-06 18:32:21 +0200

    made some changes to adapt image sizing to cloudinary

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,7 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   validates :image, content_type: {in: ["image/jpeg", "image/gif", "image/png"], message: "Must be a valid image format."}, size: {less_than: 5.megabytes, message: "Should be less than 5MB."}
 
-  def display_image
-    image.variant(resize_to_limit: [500, 500])
-  end
+  # def display_image
+  #   image.variant(resize_to_limit: [500, 500])
+  # end
 end
