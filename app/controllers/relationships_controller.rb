489b694604c63632a50c1d8442247cd557cd9596
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-11 20:26:59 +0200

    implementing follow unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:user][:id])
     current_user.follow(user)
-    redirect_to user_path(user)
+    respond_to do |format|
+      format.html {redirect_to user_path(@user)}
+      format.js
+    end
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user_path(user)
+    respond_to do |format|
+      format.html {redirect_to user_path(@user)}
+      format.js
+    end
   end
 end
--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create should require logged_in user" do
+    assert_no_difference "Relationship.count" do
+      post relationships_path
+    end
+    assert_redirected_to login_path
+  end
+
+  test "destroy should require logged_in user" do
+    assert_no_difference "Relationship.count" do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_path
+  end
 end
