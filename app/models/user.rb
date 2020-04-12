acbdcbe04afee99ecd5bca4570b5a918acb870fb
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-12 22:40:01 +0200

    took care of some front stuff and also implemented feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,11 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
   private
 
   def downcase_email
