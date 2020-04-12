dfd6d0ebf25cc048994318767255c3b161445684
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-12 20:54:09 +0200

    switching gravatar for uploaded photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships
+  has_one_attached :photo
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
